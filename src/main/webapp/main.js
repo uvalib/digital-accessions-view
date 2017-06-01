43f46bfdbcf0785b49b6a1ac46d94b50f5be97d4
Sam Crater <samcrater@ths85.net>
2017-06-01 11:20:03 -0400

    Added delete function and added empty selection detection.

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -1,9 +1,11 @@
 var imageList = [];
 
+/* Adds a row to the selection table with the specified image. */
+
 function add(img) {
 	imageList.push(img);
 	var table = document.getElementById('selectionTable');
-	var newRow = table.insertRow();
+	var newRow = table.insertRow(table.getElementsByTagName("tr").length - 1);
 	
 	var textCell = newRow.insertCell();
 	textCell.appendChild(document.createTextNode(imageList[imageList.length - 1]));
@@ -12,12 +14,52 @@ function add(img) {
 	var removeButton = document.createElement('button');
 	removeButton.type = 'button';
 	removeButton.innerHTML = 'Remove';
-	removeButton.onclick = remove; //does not show up in HTML but works
+	var attribute = document.createAttribute('onclick');
+	attribute.value = "remove(this)";
+	removeButton.setAttributeNode(attribute);
 	buttonCell.appendChild(removeButton);
+	
+	noImageCheck();
 }
 
-var remove = function() {
-	document.getElementById('test').innerHTML = 'text';
+/* Removes the row containing the calling button and removes
+the corresponding imageList entry. */
+
+function remove(object) {
+	var row = object.parentNode.parentNode;
+	
+	var content = row.nextSibling.innerHTML;
+	
+	for (i = 0; i < imageList.length; i++) {
+		if (imageList[i] == content) {
+			imageList.splice(i, 1);
+			break;
+		}
+	}
+	
+	row.remove();
+	noImageCheck();
+}
+
+/* Adds a "-None-" row to the selection table if there are no
+selected images. */
+
+function noImageCheck() {
+	var table = document.getElementById('selectionTable');
+	
+	if (table.getElementsByTagName("tr").length == 2) {
+		var noneRow = table.insertRow(table.getElementsByTagName("tr").length - 1);
+		
+		var textCell = noneRow.insertCell();
+		textCell.appendChild(document.createTextNode("-None-"));
+		textCell.colSpan = "2";
+		textCell.style.textAlign = "center";
+		
+		noneRow.appendChild(textCell);
+		noneRow.id = "noneRow";
+	} else {
+		document.getElementById("noneRow").remove();
+	}
 }
 
 function createImageSet() {
