8a026dd6254725e6085111e5976aa2a85e647f25
samuelcrater <samcrater@ths85.net>
2017-07-19 15:13:29 -0400

    Styled pages, added 'Add all' button (#6)
    * Styled pages, added 'Add all' button
    
    * Separated file viewer functionality into viewer and editor modes
    
    * replaced image set counter with static message

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -2,20 +2,49 @@ var imageList = []; //Potentially non-unique file path shown to user
 var uriList = []; //Unique URI sent to server
 
 var duplicate = false; //Flag for duplicate images in selection
+var editorVisible = false;
 var imageSetId = "";
 var imageSetUri = "";
 
 /* Disables 'Add' buttons for files that aren't images,
-loads image set if relevant, and calls noImageCheck
-to generate 'None' row. */
+loads image set if relevant, calls noImageCheck to
+generate 'None' row, and shortens file paths if needed. */
 
 function init() {
 	var fileTableRows = document.getElementsByTagName('table')[0].getElementsByTagName('tr');
-	for (i = 1; i < fileTableRows.length; i++) {
-		if (!fileTableRows[i].children[2].innerHTML.startsWith("image")) { //mime
-			fileTableRows[i].children[3].children[0].disabled = true;
+	
+	for (var i = 1; i < fileTableRows.length; i++) {
+		//shorten file path if longer than 40 characters
+		if (fileTableRows[i].children[0].children[0] != null) {
+			fileTableRows[i].children[0].children[0].innerHTML = trimFilePath(fileTableRows[i].children[0].children[0].innerHTML);
+		} else {
+			fileTableRows[i].children[0].innerHTML = trimFilePath(fileTableRows[i].children[0].innerHTML);
 		}
+		
+		//condense file sizes
+		var fileSize = parseInt(fileTableRows[i].children[1].innerHTML); //text in column 2
+		if (!isNaN(fileSize)) {
+			if (fileSize > 999999999) {
+				fileTableRows[i].children[1].innerHTML = Math.round((fileSize / 1073741824) * 10) / 10 + "GB";
+			} else if (fileSize > 999999) {
+				fileTableRows[i].children[1].innerHTML = Math.round((fileSize / 1048576) * 10) / 10 + "MB";
+			} else if (fileSize > 999) {
+				fileTableRows[i].children[1].innerHTML = Math.round((fileSize / 1024) * 10) / 10 + "KB";
+			} else {
+				fileTableRows[i].children[1].innerHTML = Math.round(fileSize * 10) / 10 + "B";
+			}
+		}
+		
+		//disable 'Add' button if the row doesn't contain an image
+		if (!fileTableRows[i].children[2].innerHTML.startsWith("image")) { //mime
+			fileTableRows[i].children[3].children[0].removeAttribute('onclick');
+			fileTableRows[i].children[3].children[0].setAttribute('class', 'disabled');
+			fileTableRows[i].children[3].children[0].children[0].setAttribute('src', '/img/disabled.png');
+		}
+		
 	}
+	
+	toggleSizeAndMime();
 	
 	document.getElementById('imageSetName').value = "";
 	
@@ -40,7 +69,9 @@ function init() {
 				
 				window.history.pushState("state", "", window.location.href.substring(0, window.location.href.indexOf("?"))); //trim off variable
 				
-				document.getElementById('p').innerHTML = "Loaded image set.";
+				toggleCreator();
+				
+				document.getElementById('p').innerHTML = "";
 			}
 		}
 		
@@ -51,8 +82,38 @@ function init() {
 		
 	}
 	
-	getNumberOfImageSets();
+	//getNumberOfImageSets();
 	noImageCheck();
+}
+
+
+
+/* Trims file path to 40 characters, maintaining file name */
+
+function trimFilePath(filePath) {
+	//shorten file path if longer than 40 characters
+	if (filePath.length > 40) {
+		var fileName;
+		
+		if (filePath.lastIndexOf("/") != -1) {
+			fileName = filePath.substring(filePath.lastIndexOf("/"));
+		} else {
+			fileName = filePath;
+		}
+		
+		//if we would chop file name
+		if (fileName.length > 18 && fileName.length < 36) {
+			//keep file name and first 36 - fileName.length characters
+			filePath = filePath.substring(0, 36 - fileName.length) + "...." + filePath.substring(filePath.length - fileName.length);
+		} else if (fileName.length >= 36) {
+			//keep just file name
+			filePath = "...." + fileName;
+		} else {
+			//keep first and last 18 characters
+			filePath = filePath.substring(0, 18) + "...." + filePath.substring(filePath.length - 18);
+		}
+	}
+	return filePath;
 }
 
 
@@ -69,7 +130,7 @@ function addRow(img, uri, mime) {
 		var table = document.getElementById('selectionTable');
 		var newRow = table.insertRow(table.getElementsByTagName("tr").length - 1);
 		
-		populateRow(newRow, img, uri);
+		populateRow(newRow, trimFilePath(img), uri);
 		checkDuplicates();
 		noImageCheck();
 	} else {
@@ -178,6 +239,15 @@ function moveRow(button, up, end) {
 
 
 
+/* Adds all images to the selection */
+
+function addAllImages() {
+	var fileTableRows = document.getElementsByTagName('table')[0].getElementsByTagName('tr');
+	for (var i = 1; i < fileTableRows.length; i++) {
+		fileTableRows[i].children[3].children[0].click();
+	}
+}
+
 /* Adds a "-None-" row to the selection table if empty and
 calls disableCreateButton. */
 
@@ -185,7 +255,7 @@ function noImageCheck() {
 	var table = document.getElementById('selectionTable');
 	var createButton = document.getElementById('createButton');
 	
-	//if selection is empty
+	//if no files are selected
 	if (table.getElementsByTagName("tr").length == 2) {
 		//add "-None-" indication row
 		var noneRow = table.insertRow(table.getElementsByTagName("tr").length - 1);
@@ -209,7 +279,7 @@ function noImageCheck() {
 
 
 
-/* Disables the createButton if there is no selection or no name. */
+/* Disables the createButton if there is no selection, no name, or duplicates. */
 
 function disableCreateButton() {
 	var table = document.getElementById('selectionTable');
@@ -229,8 +299,7 @@ function disableCreateButton() {
 /* Checks availability of image set name before calling createImageSet */
 
 function checkName() {
-	var imageSetName = document.getElementById('imageSetName').value.replace(" ", "_");
-	
+	var imageSetName = document.getElementById('imageSetName').value.replace(/ /g, '_');
 	var xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState == 4 && this.status != 200) {
@@ -265,7 +334,7 @@ function checkName() {
 function createImageSet() {
 	if (uriList.length != 0 && document.getElementById('imageSetName').value !== "" && !duplicate) {
 		var table = document.getElementById('selectionTable');
-		var imageSetName = document.getElementById('imageSetName').value.replace(" ", "_");
+		var imageSetName = document.getElementById('imageSetName').value.replace(/ /g, '_');
 		
 		//convert array to string
 		var uriString = "[ ";
@@ -290,7 +359,7 @@ function createImageSet() {
 				imageList.length = 0;
 				document.getElementById('imageSetName').value = "";
 				
-				setTimeout(function(){getNumberOfImageSets();}, 7500); //server takes time to update
+				//setTimeout(function(){getNumberOfImageSets();}, 7500); //server takes time to update
 				checkDuplicates();
 				noImageCheck();
 				
@@ -325,54 +394,62 @@ function createImageSet() {
 function populateRow(newRow, img, uri) {
 	//add movement buttons
 	var moveCell = newRow.insertCell();
-	
+	moveCell.setAttribute("class", "move");
+	/*
 	var topButton = document.createElement("button");
 	topButton.innerHTML = "&uarr;&uarr;";
-	var attribute = document.createAttribute('onclick');
-	attribute.value = "moveRow(this, true, true)";
-	topButton.setAttributeNode(attribute);
+	topButton.setAttribute('onclick', 'moveRow(this, true, true)');
 	topButton.title = "Move image to top";
 	moveCell.appendChild(topButton);
+	*/
+	var topButton = document.createElement("div");
+	topButton.setAttribute('class', 'imgButton topButton');
+	topButton.setAttribute('onclick', 'moveRow(this, true, true)');
+	topButton.title = "Move image to top";
+	topButton.appendChild(document.createElement("img"));
+	topButton.children[0].setAttribute('src', '/img/top.png');
+	moveCell.appendChild(topButton);
 	
-	var upButton = document.createElement("button");
-	upButton.innerHTML = "&uarr;";
-	attribute = document.createAttribute('onclick');
-	attribute.value = "moveRow(this, true, false)";
-	upButton.setAttributeNode(attribute);
+	var upButton = document.createElement("div");
+	upButton.setAttribute('class', 'imgButton upButton');
+	upButton.setAttribute('onclick', 'moveRow(this, true, false)');
 	upButton.title = "Move image up";
+	upButton.appendChild(document.createElement("img"));
+	upButton.children[0].setAttribute('src', '/img/up.png');
 	moveCell.appendChild(upButton);
 	
 	moveCell.appendChild(document.createElement("br"));
 	
-	var bottomButton = document.createElement("button");
-	bottomButton.innerHTML = "&darr;&darr;";
-	attribute = document.createAttribute('onclick');
-	attribute.value = "moveRow(this, false, true)";
-	bottomButton.setAttributeNode(attribute);
+	var bottomButton = document.createElement("div");
+	bottomButton.setAttribute('class', 'imgButton bottomButton');
+	bottomButton.setAttribute('onclick', 'moveRow(this, false, true)');
 	bottomButton.title = "Move image to bottom";
+	bottomButton.appendChild(document.createElement("img"));
+	bottomButton.children[0].setAttribute('src', '/img/bottom.png');
 	moveCell.appendChild(bottomButton);
 	
-	var downButton = document.createElement("button");
-	downButton.innerHTML = "&darr;";
-	attribute = document.createAttribute('onclick');
-	attribute.value = "moveRow(this, false, false)";
-	downButton.setAttributeNode(attribute);
+	var downButton = document.createElement("div");
+	downButton.setAttribute('class', 'imgButton downButton');
+	downButton.setAttribute('onclick','moveRow(this, false, false)');
 	downButton.title = "Move image down";
+	downButton.appendChild(document.createElement("img"));
+	downButton.children[0].setAttribute('src', '/img/down.png');
 	moveCell.appendChild(downButton);
 	
 	//add text
 	var textCell = newRow.insertCell();
+	textCell.setAttribute("class", "selectedFiles");
 	textCell.appendChild(document.createTextNode(img));
 	
 	//add button
 	var buttonCell = newRow.insertCell();
-	var removeButton = document.createElement('button');
-	removeButton.type = 'button';
-	removeButton.innerHTML = 'Remove';
-	attribute = document.createAttribute('onclick');
-	attribute.value = "removeRow(this)";
-	removeButton.setAttributeNode(attribute);
+	buttonCell.setAttribute("class", "remove");
+	var removeButton = document.createElement('div');
+	removeButton.setAttribute('class', 'imgButton removeButton');
+	removeButton.setAttribute('onclick','removeRow(this)');
 	removeButton.title = "Remove image from selection";
+	removeButton.appendChild(document.createElement("img"));
+	removeButton.children[0].setAttribute('src', '/img/remove.png');
 	buttonCell.appendChild(removeButton);
 	
 	//store uri info in hidden cell
@@ -472,6 +549,10 @@ function checkDuplicates() {
 	}
 }
 
+
+
+/* Gets the number of image sets and places the number in a 'p' tag */
+
 function getNumberOfImageSets() {
 	//create AJAX server request
 	var xhr = new XMLHttpRequest();
@@ -484,8 +565,8 @@ function getNumberOfImageSets() {
 			
 			document.getElementById('imageSetCount').innerHTML = "There " +
 					(Object.keys(imageSets).length == 1 ?
-					"is currently <a href=\"" + url + "\">1 image set of the current bag.</a>" :
-					"are currently <a href=\"" + url + "\">" + Object.keys(imageSets).length + " image sets of the current bag.</a>");
+					"is currently <a href=\"" + url + "\">1 image set of this bag.</a>" :
+					"are currently <a href=\"" + url + "\">" + Object.keys(imageSets).length + " image sets of this bag.</a>");
 		}
 	}
 	
@@ -495,7 +576,50 @@ function getNumberOfImageSets() {
 	document.getElementById('imageSetCount').innerHTML = "Fetching image set data...";
 }
 
+
+
+/* Called when clicking to edit image set. Loads image viewer 
+with parameters in the URL to load the proper image set. */
+
 function editImageSet(imageSetName, imageSetURI) {
 	var url = window.location.href.substring(0, window.location.href.lastIndexOf("/")) + "?edit=" + imageSetName + "&imageSetUri=" + imageSetURI;
 	window.location.href = url;
+}
+
+
+
+/* Hides or shows image set creation functionality. */
+
+function toggleCreator() {
+	editorVisible = !editorVisible;
+	
+	toggleSizeAndMime();
+	
+	var addColumn = document.getElementsByClassName("add");
+	for (var i = 0; i < addColumn.length; i++) {
+		addColumn[i].style.display = editorVisible ? "table-cell" : "none";
+	}
+	
+	document.getElementById("viewerTitle").innerHTML = editorVisible ? "Image Set Creator" : "Bag Viewer";
+	document.getElementById("editorDescription").style.display = editorVisible ? "block" : "none";
+	document.getElementById("viewerButton").innerHTML = editorVisible ? "Hide Image Set Creator" : "Open Image Set Creator";
+	document.getElementById("editorAddAll").style.display = editorVisible ? "inline" : "none";	
+	document.getElementById("selectionTable").style.display = editorVisible ? "inline-table" : "none";
+}
+
+window.onresize = function(e) {
+	toggleSizeAndMime();
+}
+
+
+
+/* Hides or shows size and mime columns when needed. */
+
+function toggleSizeAndMime() {
+	var size = document.getElementsByClassName("size");
+	var mime = document.getElementsByClassName("mime");
+	for (var i = 0; i < size.length; i++) {
+		size[i].style.display = window.innerWidth > 1125 || !editorVisible ? "table-cell" : "none";
+		mime[i].style.display = window.innerWidth > 1125 || !editorVisible ? "table-cell" : "none";
+	}
 }
